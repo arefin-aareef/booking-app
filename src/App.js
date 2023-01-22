1cfbf5965dfabe49533c67db1cfcbc337ccf58f7
arefin_aareef <arefin.aareef@gmail.com>
2023-01-22 17:32:41 +0600

    Home component created and imported to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
